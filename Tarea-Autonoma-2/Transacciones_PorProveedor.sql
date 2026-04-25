66e1279bb38b19157d10969ad141df7234fee1ca
Luis Ordoñez <gonzalo.ordonez@unach.edu.ec>
2026-04-25 00:47:57 -0500

    Delete Tarea-Autonoma-2 directory

--- Tarea-Autonoma-2/Transacciones_PorProveedor.sql
+++
@@ -1,9 +0,0 @@
-CREATE FUNCTION Transacciones_PorProveedor (@proveedor_id INT)
-RETURNS TABLE
-AS
-RETURN (
-    SELECT * FROM Transacciones 
-    WHERE proveedor_id = @proveedor_id
-);
-
-SELECT * FROM Transacciones_PorProveedor(1);
--- Tarea-Autonoma-2/Vista_ProductosActivos.sql
+++
@@ -1,7 +0,0 @@
-CREATE VIEW Vista_Productos_Activos AS
-SELECT p.producto_id, p.nombre AS Producto, p.stock, pr.nombre AS Proveedor
-FROM Productos p
-JOIN Proveedores pr ON p.proveedor_id = pr.proveedor_id
-WHERE p.stock > 0;
-
-SELECT * FROM Vista_Productos_Activos
